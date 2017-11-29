9c50e2aff3e9aadee6c8faace5172e4d41e328c1
no_author <contact@osivia.com>
2017-11-29 14:30:47 +0000

    Fixes developerConnection

--- opentoutatice-addon-elasticsearch-ecm/src/main/java/fr/toutatice/ecm/elasticsearch/servlet/VcsToEsQueryFilter.java
+++ opentoutatice-addon-elasticsearch-ecm/src/main/java/fr/toutatice/ecm/elasticsearch/servlet/VcsToEsQueryFilter.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang.StringUtils;
 import org.nuxeo.ecm.automation.core.operations.services.DocumentPageProviderOperation;
 import org.nuxeo.ecm.automation.core.operations.services.query.DocumentPaginatedQuery;
-import org.nuxeo.runtime.api.Framework;
 
 /**
  * @author david
@@ -24,59 +23,58 @@ import org.nuxeo.runtime.api.Framework;
  */
 public class VcsToEsQueryFilter implements Filter {
 
-	/**
-	 * Exception of QUERYING_FORCE_ES: if set to true, force given request in VCS.
-	 */
-	public static final String QUERYING_VCS_FORCE_FLAG = "nx_querying_vcs_force";
+    /**
+     * Exception of QUERYING_FORCE_ES: if set to true, force given request in VCS.
+     */
+    public static final String QUERYING_VCS_FORCE_FLAG = "nx_querying_vcs_force";
 
-	/** Operation resource of Document.QueryES. */
-	private static final String QUERY_ES_OP_RESOURCE = "/site/automation/Document.QueryES";
+    /** Operation resource of Document.QueryES. */
+    private static final String QUERY_ES_OP_RESOURCE = "/site/automation/Document.QueryES";
 
-	/**
-	 * Document.QueryES compatibility mode (set schema in parameter is deprecated).
-	 */
-	public static final String QUERY_ES_COMPAT_MODE = "qEsCompat";
+    /**
+     * Document.QueryES compatibility mode (set schema in parameter is deprecated).
+     */
+    public static final String QUERY_ES_COMPAT_MODE = "qEsCompat";
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		// Nothing
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Nothing
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		// Case of not HttpServeltRequest
-		if (request instanceof HttpServletRequest == false) {
-			chain.doFilter(request, response);
-			return;
-		}
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // Case of not HttpServeltRequest
+        if (request instanceof HttpServletRequest == false) {
+            chain.doFilter(request, response);
+            return;
+        }
 
-		HttpServletRequest httpReq = (HttpServletRequest) request;
+        HttpServletRequest httpReq = (HttpServletRequest) request;
 
-		// Get ElasticSearch querying mode from header
-		final boolean queryingVcs = Boolean.valueOf(httpReq.getHeader(QUERYING_VCS_FORCE_FLAG));
+        // Get ElasticSearch querying mode from header
+        final boolean queryingVcs = Boolean.valueOf(httpReq.getHeader(QUERYING_VCS_FORCE_FLAG));
 
-		// Called operation
-		String opId = StringUtils.substringAfterLast(httpReq.getPathInfo(), "/");
+        // Called operation
+        String opId = StringUtils.substringAfterLast(httpReq.getPathInfo(), "/");
 
-		if (!queryingVcs) {
-			// Check operation
+        if (!queryingVcs) {
+            // Check operation
 
-			if (DocumentPaginatedQuery.ID.equals(opId) || DocumentPageProviderOperation.ID.equals(opId)) {
-				// Redirect all Document.Query by Document.QueryES
-				httpReq.getRequestDispatcher(QUERY_ES_OP_RESOURCE).forward(request, response);
-			} else {
-				chain.doFilter(request, response);
-			}
+            if (DocumentPaginatedQuery.ID.equals(opId) || DocumentPageProviderOperation.ID.equals(opId)) {
+                // Redirect all Document.Query by Document.QueryES
+                httpReq.getRequestDispatcher(QUERY_ES_OP_RESOURCE).forward(request, response);
+            } else {
+                chain.doFilter(request, response);
+            }
 
-		} else {
-			chain.doFilter(request, response);
-		}
-	}
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
 
-	@Override
-	public void destroy() {
-		// Nothing
-	}
+    @Override
+    public void destroy() {
+        // Nothing
+    }
 
 }
